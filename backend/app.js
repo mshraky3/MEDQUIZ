f9be1a64a89d6875dede7b39ca0b92bc6e2961e8
mahmod alshraky <93828510+mshraky3@users.noreply.github.com>
2025-06-11 00:28:48 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,6 @@ const db = new Pool({
     ssl: {
         rejectUnauthorized: false
     },
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 5000
 });
 
 const app = express();
