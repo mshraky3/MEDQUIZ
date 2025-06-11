a0bb91afe9665cb66dc003c98a598ff995c0d864
mahmod alshraky <93828510+mshraky3@users.noreply.github.com>
2025-06-11 06:01:18 +0300

    Add files via upload

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer() // Optional: see bundle size breakdown
+  ],
+  build: {
+    target: 'es2022', // modern JS support
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')
+              .shift()
+              .replace('.js', '')
+          }
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }
 })
