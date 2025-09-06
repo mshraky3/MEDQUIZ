f902925ebfb16d70ae39781eeb9e5074ac442596
mahmod alshraky <93828510+mshraky3@users.noreply.github.com>
2025-09-07 00:53:38 +0300

    Add files via upload

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,12 +3,14 @@ import Landing from './components/landing/Landing';
 import Navbar from './components/common/Navbar.jsx';
 import { useLocation } from 'react-router-dom';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
     const location = useLocation();
     const showNavbar = location.pathname !== '/';
     return (
         <div style={{ minHeight: '100vh', background: '#f8fafc', margin: 0, padding: 0 }}>
+            <SpeedInsights />
             {process.env.NODE_ENV === 'production' && <Analytics />}
             {showNavbar && <Navbar />}
             <Landing />
